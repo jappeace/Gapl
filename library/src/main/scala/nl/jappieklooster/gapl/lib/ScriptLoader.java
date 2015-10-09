7730adab802fc3a014070432ce6e4f219c4a9be0
Jappie Klooster <jappieklooster@hotmail.com>
2015-10-09 22:43:36 +0200

    trying to add a logger for the script loader

---
+++ library/src/main/scala/nl/jappieklooster/gapl/lib/ScriptLoader.java
@@ -0,0 +1,56 @@
+package nl.jappieklooster.gapl.lib;
+
+import groovy.lang.Binding;
+import groovy.lang.GroovyShell;
+import groovy.util.DelegatingScript;
+import org.codehaus.groovy.control.CompilationFailedException;
+import org.codehaus.groovy.control.CompilerConfiguration;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Path;
+
+/**
+ * generic script loader. abstracts away the anucances of script loading
+ *
+ * if you load the same dsl on different locations you should make a specilized instance in this package and
+ * inject that
+ */
+public class ScriptLoader {
+
+	private GroovyShell shell;
+	public ScriptLoader(){
+        final CompilerConfiguration compilerConfig = new CompilerConfiguration();
+        compilerConfig.setScriptBaseClass(DelegatingScript.class.getName());
+        shell = new GroovyShell(new Binding(),compilerConfig);
+	}
+
+	/**
+	 * handles the little pains of loading a groovy script
+	 * may fail misserably silently
+	 * @param path
+	 * @param delagate this object will handle the dsl calls
+	 * @return succes
+	 */
+	public boolean load(final Path path, Object delagate){
+		//Log.info("loadin script: " + path);
+		boolean success = false;
+		final File file = path.toFile();
+		if(!file.isFile()){
+			//Log.warn("script file is not a file: " + path + " \n pwd: " + new File(".").getAbsolutePath());
+			return success;
+		}
+
+		try {
+			final DelegatingScript script = (DelegatingScript)shell.parse(file);
+            script.setDelegate(delagate);
+            script.run();
+            success = true;
+		} catch (final CompilationFailedException e) {
+			e.printStackTrace();
+		} catch (final IOException e) {
+			e.printStackTrace();
+		}
+		return success;
+	}
+}

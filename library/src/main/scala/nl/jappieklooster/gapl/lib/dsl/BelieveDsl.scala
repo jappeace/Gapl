823406424e392d2a556647d0fd058e857c447ce5
Jappie Klooster <jappieklooster@hotmail.com>
2015-10-13 10:27:10 +0200

    simplified believes, I think this will make it easier to work with
    I wanted to seperate them because they have to do differetn things
    eventually. But after thinking about it I think its better to handle
    the way these are handled on a later level because it would allow
    unexpected behavior to be introduced (what if you create a value believa
    and an induction of the same name, with this simpeler setup the last one
    gets overwritten, which is excpected)

--- library/src/main/scala/nl/jappieklooster/gapl/lib/dsl/BelieveDsl.scala
+++ library/src/main/scala/nl/jappieklooster/gapl/lib/dsl/BelieveDsl.scala
@@ -1,6 +1,5 @@
 package nl.jappieklooster.gapl.lib.dsl
 
-import groovy.lang.Closure
 import nl.jappieklooster.gapl.lib.model.Agent
 import nl.jappieklooster.groovy.meta.IMissingMethodHandler
 import org.slf4j.{Logger, LoggerFactory}
@@ -9,15 +8,6 @@ class BelieveDsl(var subject:Agent) extends IMissingMethodHandler{
 	private val log: Logger = LoggerFactory.getLogger(classOf[BelieveDsl])
 
 	def methodMissing(name: String, args: Array[AnyRef]): Unit= {
-		if(args.length == 1){
-			args.apply(0) match {
-				case closure: Closure[Any] =>
-					log.info("closure....")
-					subject = subject.copy(deductions = subject.deductions + (name -> closure))
-					return
-				case _ =>
-			}
-		}
 		subject = subject.copy(believes = subject.believes + (name -> args))
 	}
 }
--- library/src/main/scala/nl/jappieklooster/gapl/lib/model/Agent.scala
+++ library/src/main/scala/nl/jappieklooster/gapl/lib/model/Agent.scala
@@ -1,14 +1,11 @@
 package nl.jappieklooster.gapl.lib.model
 
-import groovy.lang.Closure
-
 /**
  * believes are just data
  * deductions is data derived from the beleives
  */
 case class Agent(
-	believes : Map[String, Iterable[Any]],
-	deductions :Map[String, Closure[Any]]
+	believes : Map[String, Iterable[Any]]
 ) {
-	def this() = this(Map(), Map())
+	def this() = this(Map())
 }
